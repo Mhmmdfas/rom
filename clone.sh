370bb0de577119ec2a54572f2d93e34d3108e4a7
Mhmmdfas <57018840+Mhmmdfas@users.noreply.github.com>
2019-11-26 12:13:05 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -6,4 +6,4 @@ BRANCH="lineage-16.0"
 
 git clone -b $BRANCH $GITHUB/android_device_xiaomi_rolex device/xiaomi/rolex
 git clone -b $BRANCH $GITHUB/android_vendor_xiaomi vendor/xiaomi/rolex
-git clone -b $BRANCH -j32 --depth=1 $GITHUB/android_kernel_xiaomi_msm8917 kernel/xiaomi/msm8917
+git clone -b $BRANCH --depth=1 $GITHUB/android_kernel_xiaomi_msm8917 kernel/xiaomi/msm8917
